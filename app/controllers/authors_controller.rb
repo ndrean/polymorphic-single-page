c5056bf90e3b1c47432643e5169035eb092ad5f6
Neven DREAN <nevendrean@yahoo.fr>
2020-04-06 00:00:43 +0200

    unique tag per book per author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,23 +7,17 @@ class AuthorsController < ApplicationController
     @author = Author.new
     @author.books.build
     @author.books.build
-    
-    
-    #@author.books.build # to create another one
-
-    
     # since the 'simple_field_for' iterates on all books, we can create
     # another related book when adding another time
-    
+    #@author.books.build
     
     @genres =  Genre.all
     @genre = Genre.new
-    
   end
 
   def create
     @author = Author.new(author_params)
-    #local_binding
+    logger.debug ".............................create"
     if @author.save
       redirect_to root_path
     else
